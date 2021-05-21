0d84325d55ed1235c5e4e0bd39a9d503e752c049
moziada <moziada@users.noreply.github.com>
2021-05-21 18:32:38 +0200

    Update colorize_folder.py
    refractor folder colorization

--- colorize_folder.py
+++ colorize_folder.py
@@ -14,18 +14,19 @@ for imgName in os.listdir(input_dir):
     save_path = os.path.join(output_folder, imgName.split('.')[0])
 
     pil_img = read_to_pil(os.path.join(input_dir, imgName))
+    pil_img = resize_large_img(pil_img)
+
+    outputs = detector(pil_img, save_path)
+    masks = instancesMasks(pil_img.size, outputs)
+    pred_bboxes = outputs["instances"].pred_boxes.to(torch.device('cpu')).tensor.numpy().astype(np.int32)
+    instances_indecies = segnificat_bboexes_indices(pil_img, pred_bboxes, Threshold=0.001)
 
     (HW_orig, tens_l_img_rs) = preprocess(pil_img, HW=(256, 256))
     tens_ab_img_rs = colorizer_eccv16(tens_l_img_rs.cuda()).cpu()
     tens_ab_img = scaleback_ab_tens(HW_orig, tens_ab_img_rs)
     np_ab_img = tens2np(tens_ab_img)
 
-    np_grey_img_3Ch = pil_to_gray_np(pil_img, Channels=3)
-    outputs = detector(np_grey_img_3Ch, save_path)
-    masks = instancesMasks(np_grey_img_3Ch.shape, outputs)
-    pred_bboxes = outputs["instances"].pred_boxes.to(torch.device('cpu')).tensor.numpy().astype(np.int32)
-
-    for i in range(len(outputs["instances"])):
+    for i in instances_indecies:
         (HW_orig_instance, tens_l_instance_rs) = preprocess(pil_img.crop(pred_bboxes[i]), HW=(256, 256))
         tens_ab_instance_rs = colorizer_eccv16(tens_l_instance_rs.cuda()).cpu()
         tens_ab_instance = scaleback_ab_tens(HW_orig_instance, tens_ab_instance_rs)
@@ -33,4 +34,4 @@ for imgName in os.listdir(input_dir):
         np_ab_img = patchFullimg(np_ab_img, np_ab_instance, pred_bboxes[i], masks[i])
 
     finalimg = postprocess(pil_img, np_ab_img, Desaturate=True)
-    save_img(save_path, finalimg)
+    save_img(save_path+'_PatchColorization', finalimg)
